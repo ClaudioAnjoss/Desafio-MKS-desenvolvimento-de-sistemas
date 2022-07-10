6d60f18318dcdd5a55e7da166f0dadf51e1d2dbf
Claudio Anjos <claudio.anjoss@outlook.com>
2022-07-10 17:18:49 -0300

    erro Access-Controll-Allow-Origin

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,18 +1,51 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import { Card } from '../Card';
+import { CorsRequest } from 'cors';
+
 import './styles.css';
 
 export function Main() {
+    const [products, setProducts] = useState();
+
+    useEffect(() => {
+        // axios.get('https://mks-frontend-challenge-api.herokuapp.com/api/v1/products?page=1&rows=50&sortBy=id&orderBy=DESC', { headers:  'Access-Control-Allow-Origin: *' } )
+        //     .then((response) => setProducts(response.data))
+        //     .catch((err) => {
+        //         console.error("ops! ocorreu um erro" + err);
+        //     });
+
+        // console.log('lista de produtos:  ' + products)
+
+        const url = 'https://mks-frontend-challenge-api.herokuapp.com/api/v1/products?page=1&rows=50&sortBy=id&orderBy=DESC';
+
+
+
+        axios
+            .get(url)
+            .then((response) => setProducts(response.data))
+            .catch(err => console.log('err', err));
+
+        console.log('lista de produtos:  ' + products)
+
+    }, [])
+
     return (
         <div className="content__main">
             <div className="content__card">
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                <Card
+                    key="1"
+                    title="Apple Watch Series 4 GPS"
+                    description="Redesigned from scratch and completely revised."
+                    price="399"
+                />
+                <Card
+                    key="2"
+                    title="JBL Speaker"
+                    description="Redesigned from scratch and completely revised."
+                    price="199"
+                />
+
             </div>
         </div>
     )
