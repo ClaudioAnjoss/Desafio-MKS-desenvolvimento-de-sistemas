7196f88772309a955c0a54b2e816e1b347aa27cc
Claudio Anjos <claudio.anjoss@outlook.com>
2022-07-11 21:16:03 -0300

    redux

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,28 +1,49 @@
-import { Card } from '../Card';
+import Card from '../Card';
 import { connect } from 'react-redux';
 
 import './styles.css';
+import { useEffect, useState } from 'react';
+import { GetProducts } from '../../scripts/GetProdutos';
 
-const Main = (module) => {
 
-    console.log(module)    
 
-    // module().then(e => {console.log(e)})
-    return (
-        <div className="content__main">
-            <div className="content__card">
-                <button onClick={() => console.log(module)} >teste</button>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+const Main = (listProducts) => {
+    const [product, SetProducts] = useState([])
+    const teste = product.map(e => {
+        return e
+    })
+    listProducts = [...teste]
+
+    useEffect(() => {
+        GetProducts().then(e => {
+            SetProducts([...e.products])
+        })
+    }, [])
+
+    if (product.length > 1) {
+        return (
+            <div className="content__main">
+                <div className="content__card">
+                    {listProducts.map(res => {
+                        return (
+                            <Card
+                                key={res.id}
+                                id={res.id}
+                                name={res.name}
+                                photo={res.photo}
+                                price={res.price}
+                                description={res.description}
+                            />
+                        )
+                    })}
+
+                </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        console.log('carregando')
+    }
+
 }
 
-export default connect(state => ({ module: state}))(Main)
+export default connect(state => ({ listProducts: state }))(Main)
--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,23 +1,10 @@
 import { createStore } from 'redux';
 import { useState } from 'react';
-
-import axios from 'axios';
-
-const url = 'https://mks-frontend-challenge-api.herokuapp.com/api/v1/products?page=1&rows=50&sortBy=id&orderBy=DESC';
-
-export const GetProducts = async products => {
-    await axios.get(url, { headers: { 'Access-Control-Allow-Origin': true } })
-        .then((response) => {
-            products = response.data
-        })
-        .catch(err => console.log('err', err))
-    return products
-}
+import { GetProducts } from '../scripts/GetProdutos';
    
 
 const reducer = () => {
-
-    // console.log(GetProducts())
+   return ([])
 }
 
 const store = createStore(reducer);
